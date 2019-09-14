3d87e306c540ba7697d51d4023dd3cb1434e271c
Ivan <i.abramov@innopolis.university>
2019-09-14 12:46:06 +0300

    improve place order

--- services/place_order/configure.py
+++ services/place_order/configure.py
@@ -6,16 +6,24 @@ from services.place_order.functions import *
 def setup(updater):
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(Filters.regex("Заказать отсутсвующую продукцию"), choose_product))
+    dispatcher.add_handler(MessageHandler(Filters.regex("Заказать отсутсвующую продукцию"), choose_product))
 
-
-    choose_product_handler = ConversationHandler(
-        entry_points=[MessageHandler(Filters.regex("Заказать отсутсвующую продукцию"), choose_product)],
+    choose_product_text_handler = ConversationHandler(
+        entry_points=[MessageHandler(Filters.regex("Отправить название"), input_text)],
         states={
-            CHOOSE_PRODUCT_TEXT: [MessageHandler(Filters.regex("^((?!Отменить).)*$"), send_product_query)],
-            CHOOSE_PRODUCT_PHOTO: [MessageHandler(Filters.photo, send_product_query)],
+            ADD_TEXT: [MessageHandler(Filters.regex("^((?!Отменить).)*$"), send_product_text)],
         },
         fallbacks=[MessageHandler(Filters.regex("Отменить"), cancel)]
     )
 
-    dispatcher.add_handler(choose_product_handler)
+    choose_product_photo_handler = ConversationHandler(
+        entry_points=[MessageHandler(Filters.regex("Отправить фото и название"), input_photo)],
+        states={
+            ADD_PHOTO: [MessageHandler(Filters.photo, send_product_photo)],
+            ADD_PHOTO_TEXT: [MessageHandler(Filters.regex("^((?!Отменить).)*$"), send_product_text)],
+        },
+        fallbacks=[MessageHandler(Filters.regex("Отменить"), cancel)]
+    )
+
+    dispatcher.add_handler(choose_product_text_handler)
+    dispatcher.add_handler(choose_product_photo_handler)
--- services/place_order/functions.py
+++ services/place_order/functions.py
@@ -4,7 +4,7 @@ from services.initial.configure import menu
 from telegram.ext import ConversationHandler
 import os
 
-CHOOSE_PRODUCT_TEXT, CHOOSE_PRODUCT_PHOTO = 0
+ADD_TEXT, ADD_PHOTO, ADD_PHOTO_TEXT = range(3)
 
 
 def choose_product(update, context):
@@ -22,12 +22,54 @@ def choose_product(update, context):
                                        one_time_keyboard=False,
                                        resize_keyboard=True)
 
-    bot.send_message(update.message.chat_id, "Напишите название продукта, который вы хотите заказать", reply_markup=reply_markup)
+    bot.send_message(update.message.chat_id, "Выберите опцию", reply_markup=reply_markup)
 
-    return CHOOSE_PRODUCT_TEXT
 
-def send_product_query(update, context):
-    #logger.info(update.message.from_user.username)
+def input_text(update, context):
+    bot = context.bot
+
+    bot = context.bot
+    # logger.info(update.message.from_user.username)
+
+    keyboard = [
+        [KeyboardButton("Отменить")],
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard=False,
+                                       resize_keyboard=True)
+
+    bot.send_message(update.message.chat_id, "Напишите названию продукта", reply_markup=reply_markup)
+
+    return ADD_TEXT
+
+
+def input_photo(update, context):
+
+    bot = context.bot
+    # logger.info(update.message.from_user.username)
+
+    keyboard = [
+        [KeyboardButton("Отменить")],
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard=False,
+                                       resize_keyboard=True)
+
+    bot.send_message(update.message.chat_id, "Отправте фотографию продукта", reply_markup=reply_markup)
+
+    return ADD_PHOTO
+
+def input_product_text(update, context):
+    bot = context.bot
+    # logger.info(update.message.from_user.username)
+
+
+
+
+
+
+def send_product_text(update, context):
+    # logger.info(update.message.from_user.username)
 
     bot = context.bot
 
@@ -41,6 +83,27 @@ def send_product_query(update, context):
     bot.send_message(update.message.chat_id, "Мы приняли ваш заказ")
     menu(update, context)
     return ConversationHandler.END
+
+
+def send_product_photo(update, context):
+    bot = context.bot
+    # logger.info(update.message.from_user.username)
+
+    user = update.message.from_user
+    first_name = user.first_name if user.first_name is not None else ""
+    last_name = user.last_name if user.last_name is not None else ""
+    username = "@" + user.username if user.username is not None else ""
+
+    photo_file_id = update.message.photo[len(update.message.photo) - 1].file_id
+    message = f"""Заказ продуктов\nот {first_name} {last_name} {username}\n"""
+
+    bot.send_photo(photo=photo_file_id, chat_id=os.environ["WORKERS_CHANNEL"], caption=message)
+
+    bot.send_message(update.message.chat_id, "Отправте название продукта")
+
+    #menu(update, context)
+
+    return ADD_PHOTO_TEXT
 
 
 def cancel(update, context):
