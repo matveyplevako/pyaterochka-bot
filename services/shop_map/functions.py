4a51cdfd87c5a09c80eadffbc5949655bfa4339c
matveyplevako <matveyplevako@rambler.ru>
2019-09-28 17:34:06 +0300

    sending map as a file

--- services/shop_map/functions.py
+++ services/shop_map/functions.py
@@ -6,6 +6,6 @@ import os
 
 def show_map(update, context):
     bot = context.bot
-    bot.send_photo(chat_id=update.message.chat_id, photo=open('services/shop_map/shop_map.jpg', 'rb'))
+    bot.send_document(chat_id=update.message.chat_id, document=open('services/shop_map/shop_map.jpg', 'rb'))
     menu(update, context)
     return ConversationHandler.END
